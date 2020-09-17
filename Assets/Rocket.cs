04737433527fa37c26bd5aaf3742a33b176e74a1
cmade <cmangerere@gmail.com>
2020-09-17 17:27:45 -0400

    Added the ability to launch the rocket.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            rigidbody.AddRelativeForce(Vector3.up);
             print("Thrusting");
         }
 
