a1baced276457089f62dbdbd7fb84a0bf1aeaaf2
cmade <cmangerere@gmail.com>
2020-09-23 19:01:58 -0400

    Added two obstacles and froze the rotation of the Rocket.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,35 +5,59 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
+    AudioSource myAudioSource;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        myAudioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
             print("Thrusting");
+            if (!myAudioSource.isPlaying)
+            {
+                myAudioSource.Play();
+            }
         }
+        else
+        {
+            myAudioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // Take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
             print("Rotating Left");
-        }else if (Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.back);
             print("Rotating Right");
         }
+        rigidbody.freezeRotation = false; // Resume physics control of rotation
     }
+
+    
 }
